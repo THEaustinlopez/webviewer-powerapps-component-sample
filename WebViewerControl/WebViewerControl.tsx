61369927e7af892879aa637188a3bc16ca2d1b8a
Austin Lopez <61397322+THEaustinlopez@users.noreply.github.com>
2024-07-11 20:01:42 +0000

    converted to class component

--- WebViewerControl/WebViewerControl.tsx
+++ WebViewerControl/WebViewerControl.tsx
@@ -8,45 +8,80 @@ interface IWebViewerControlProps {
   onDocSave: (docUrl: string) => void;
 }
 
-const WebViewerControl: React.FC<IWebViewerControlProps> = ({ docUrl, viewerHeight, viewerWidth, onDocSave }) => {
-  const viewerElementRef = React.useRef<HTMLDivElement>(null);
+interface IWebViewerControlState {
+  currentDocUrl: string;
+}
 
-  React.useEffect(() => {
-    const viewerElement = viewerElementRef.current;
+class WebViewerControl extends React.Component<IWebViewerControlProps, IWebViewerControlState> {
+  private viewerElementRef: React.RefObject<HTMLDivElement>;
+
+  constructor(props: IWebViewerControlProps) {
+    super(props);
+    this.viewerElementRef = React.createRef();
+    this.state = {
+      currentDocUrl: props.docUrl,
+    };
+  }
+
+  componentDidMount() {
+    const viewerElement = this.viewerElementRef.current;
 
     if (viewerElement) {
       WebViewer(
         {
           path: 'http://localhost:3000/lib',
           config: 'http://localhost:3000/config.js',
-          initialDoc: docUrl,
+          initialDoc: this.props.docUrl,
         },
         viewerElement
       ).then(instance => {
         const iframeWindow = viewerElement.querySelector('iframe')!.contentWindow!;
 
-        window.addEventListener('message', receiveMessage, false);
-
-        function receiveMessage(event: any) {
-          if (event.isTrusted && typeof event.data === 'object') {
-            switch (event.data.type) {
-              case 'SAVE_DOCUMENT':
-                onDocSave(event.data.payload.file);
-                break;
-              default:
-                break;
-            }
-          }
-        }
+        window.addEventListener('message', this.receiveMessage, false);
       });
     }
+  }
 
-    return () => {
-      window.removeEventListener('message', receiveMessage);
+  componentWillUnmount() {
+    window.removeEventListener('message', this.receiveMessage);
+  }
+
+  componentDidUpdate(prevProps: IWebViewerControlProps) {
+    if (prevProps.docUrl !== this.props.docUrl) {
+      this.setState({ currentDocUrl: this.props.docUrl });
+      this.handleDocOpen(this.props.docUrl);
+    }
+  }
+
+  handleDocOpen = (docUrl: string): void => {
+    const payload = {
+      file: docUrl
     };
-  }, [docUrl]);
+    this.viewerElementRef.current?.querySelector('iframe')!.contentWindow!.postMessage({ type: 'OPEN_DOCUMENT', payload }, '*');
+  };
 
-  return <div ref={viewerElementRef} style={{ height: viewerHeight + 'px', width: viewerWidth + 'px' }}></div>;
-};
+  handleDocSave = (docUrl: string): void => {
+    this.props.onDocSave(docUrl);
+  };
+
+  receiveMessage = (event: any) => {
+    if (event.isTrusted && typeof event.data === 'object') {
+      switch (event.data.type) {
+        case 'SAVE_DOCUMENT':
+          this.handleDocSave(event.data.payload.file);
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div ref={this.viewerElementRef} style={{ height: this.props.viewerHeight + 'px', width: this.props.viewerWidth + 'px' }}></div>
+    );
+  }
+}
 
 export default WebViewerControl;
+
